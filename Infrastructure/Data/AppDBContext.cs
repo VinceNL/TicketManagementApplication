9413a8f4a8124b337920d5533967acc31d8ea1ac
vvancampen <vvancampen@msn.com>
2024-11-23 21:24:00 +0100

    New DB Entities added

--- Infrastructure/Data/AppDBContext.cs
+++ Infrastructure/Data/AppDBContext.cs
@@ -9,5 +9,27 @@ namespace Infrastructure.Data
         public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
         {
         }
+
+        DbSet<Ticket> Tickets { get; set; }
+        DbSet<Product> Products { get; set; }
+        DbSet<Priority> Priorities { get; set; }
+        DbSet<Discussion> Discussions { get; set; }
+        DbSet<Category> Categories { get; set; }
+        DbSet<Attachment> Attachments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Ticket>()
+                .HasOne(t => t.User)
+                .WithMany()
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.Entity<Discussion>()
+                .HasOne(d => d.Ticket)
+                .WithMany()
+                .OnDelete(DeleteBehavior.NoAction);
+        }
     }
 }
