7b3b402a7f88fe5ab31f3b08b1297f10755cd589
vvancampen <vvancampen@msn.com>
2024-12-22 22:23:07 +0100

    ChangePassword and UploadAvatar implemented

--- Infrastructure/Common/Constants.cs
+++ Infrastructure/Common/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public const string DEFAULT_PASSWORD = "StrongP@ssw0rd!";
+        public const string DEFAULT_PASSWORD = "StrongP@ssw0rd!!";
 
         public const string STATUS_OPEN = "OPEN";
         public const string STATUS_NEW = "NEW";
